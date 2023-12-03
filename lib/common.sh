e361a26bcfd8ccdce05e5c39e6f800a024d6ec4f
pmeinhardt <mnhrdt@gmail.com>
2023-12-03 18:06:35 +0100

    Refactor scripts

--- lib/common.sh
+++ lib/common.sh
@@ -1,3 +1,16 @@
+# shellcheck shell=bash
+
+YEAR="2023"
+BASE="https://adventofcode.com/$YEAR"
+
+view () {
+  open "$BASE/$1"
+}
+
+load () {
+  curl --silent --show-error "${@:2}" "$BASE/$1"
+}
+
 quit () {
   echo "$1" >&2
   exit "${2:-0}"
